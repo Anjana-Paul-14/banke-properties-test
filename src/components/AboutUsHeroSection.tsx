a61c0c3757478a2b4affd68fe72701bcdc594a04
Anjana Paul <anjanapaul0614@gmail.com>
2025-11-13 18:05:46 +0530

    about us hero section

--- src/components/AboutUsHeroSection.tsx
+++ src/components/AboutUsHeroSection.tsx
@@ -11,7 +11,6 @@ export const AboutUsHeroSection: React.FC = () => {
       <div className="absolute inset-0 bg-black/60"></div>
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center px-4 md:px-0 max-w-4xl">
-        
           <h1 className="text-white text-1xl md:text-3xl leading-tight md:leading-tight font-extralight">
             "Where vision meets opportunity, Banke drives <br /> success through innovation and excellence"
           </h1>
