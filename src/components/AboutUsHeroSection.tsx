4566f83aa8c7cca4946af36e5fc4dac5554e3ac4
Anjana Paul <anjanapaul0614@gmail.com>
2025-11-13 18:05:32 +0530

    about us her section

--- src/components/AboutUsHeroSection.tsx
+++ src/components/AboutUsHeroSection.tsx
@@ -9,10 +9,9 @@ export const AboutUsHeroSection: React.FC = () => {
         className="w-full h-full object-cover"
       />
       <div className="absolute inset-0 bg-black/60"></div>
-
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center px-4 md:px-0 max-w-4xl">
-        {/* font-weight thin */}
+        
           <h1 className="text-white text-1xl md:text-3xl leading-tight md:leading-tight font-extralight">
             "Where vision meets opportunity, Banke drives <br /> success through innovation and excellence"
           </h1>
